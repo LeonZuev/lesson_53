14f0db76e9506fc6c62fabf1721678d358c461c4
Leon Zuev <loonatune@gmail.com>
2023-04-02 23:19:03 +0300

    Add completed Main, empty TicTacToe with empy run

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+  // Игра в крестики-нолики
+  // если игра завершена, можем спросить, начать ли новую игру
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("=== Крестики-нолики ===");
+    TicTacToe game = new TicTacToe();
+    do {
+      String result = game.run(br);
+      //когда метод завершится, игра закончится
+      System.out.println(result);
+      System.out.println("Хотите повторить? [y/n]");
+    } while (readYesOrNo(br.readLine())); // падение при некорректном вводе - ожидаемое поведение
+    System.out.println("До свиданья!");
+  }
+
+  private static boolean readYesOrNo(String answer) {
+    return switch (answer.toLowerCase()) {
+      case "y", "yes", "да", "true", "1" -> true;
+      case "n", "no", "нет", "false", "0" -> false;
+      default -> // все остальные случаи
+      throw new IllegalArgumentException("Некорректный ответ, введите да или нет: " + answer);
+    };
   }
 }
---
+++ src/TicTacToe.java
@@ -0,0 +1,7 @@
+import java.io.BufferedReader;
+
+public class TicTacToe {
+  public String run(BufferedReader br) {
+    return null;
+  }
+}
