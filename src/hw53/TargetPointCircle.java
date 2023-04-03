4c52ed237118e42484beeba7fb0ada46db83558c
Leon Zuev <loonatune@gmail.com>
2023-04-03 05:39:36 +0300

    Add TargetPointCircle with description

---
+++ src/hw53/TargetPointCircle.java
@@ -0,0 +1,19 @@
+package hw53;
+
+import cw53.Point;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Получите от пользователя радиус окружности.
+По введённым координатам точки определите,
+попадает ли она внутрь, на границу или вне окружности с
+указанным радиусом и центром в точке O (0, 0).
+x^2 + y^2 < r^2 - внутрь
+x^2 + y^2 = r^2 - на границу
+ */
+
+public class TargetPointCircle {
+}
