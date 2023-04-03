d8bb50f69782b541fd75781137d35a5c516ab56f
Leon Zuev <loonatune@gmail.com>
2023-04-03 06:20:45 +0300

    Finish TargetPointCircle

--- src/hw53/TargetPointCircle.java
+++ src/hw53/TargetPointCircle.java
@@ -16,4 +16,25 @@ x^2 + y^2 = r^2 - на границу
  */
 
 public class TargetPointCircle {
+
+
+  public static String targetCircle(Point point) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    int x = point.getX();
+    int y = point.getY();
+    System.out.println("Введите границы заграницы");
+    int r = Integer.parseInt(bufferedReader.readLine());
+    ;
+
+    if (x * x + y * y < r * r) {
+      return "Точка внутри окружности";
+    }
+    else if (x * x + y * y == r * r) {
+      return "Точка на границе окружности";
+    }
+    else {
+      return "Точка за границей, там тепло";
+    }
+  }
 }
+
